eb7867cecba61461004c616297c0714ccb889c98
marieleponti <mariele.ponticiello@temple.edu>
2026-02-17 13:52:44 -0500

    Log Out button, functional login & logout, login reqd

--- src/nat_state_vis_app/urls.py
+++ src/nat_state_vis_app/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
 ]

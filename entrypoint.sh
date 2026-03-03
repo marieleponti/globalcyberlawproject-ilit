06f0975bb854cbe40be9a9146635b7adabdf0c1a
marieleponti <mariele.ponticiello@temple.edu>
2026-03-03 10:54:15 -0500

    correct entrypoint with nat_state_vis_app replace <config> and add <static root> base dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Levanta Gunicorn en el puerto que asigna Railway
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+exec gunicorn nat_state_vis_app.wsgi:application --bind 0.0.0.0:$PORT

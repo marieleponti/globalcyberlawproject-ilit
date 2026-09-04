d7317c61f144c48cc018af6ee9769dbca1e27d48
marieleponti <mariele.ponticiello@temple.edu>
2026-09-04 13:35:51 -0400

    add <email timeout> set to 10 in settings.py

--- src/nat_state_vis_app/settings.py
+++ src/nat_state_vis_app/settings.py
@@ -180,6 +180,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 CONTACT_RECIPIENT_EMAIL = os.environ.get("CONTACT_RECIPIENT_EMAIL", EMAIL_HOST_USER)
+EMAIL_TIMEOUT = 10
 
 #To allow contact form submission on Render
 CSRF_TRUSTED_ORIGINS = ['https://globalcyberlaw.onrender.com']
